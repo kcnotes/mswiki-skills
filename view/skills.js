dd6da434ac5684deaee01e007d228bbc142f766d
PirateIzzy <izzydaknight@gmail.com>
2021-10-06 22:20:21 -0400

    GMS v227

--- view/skills.js
+++ view/skills.js
@@ -100,7 +100,7 @@
     let skillData;
     let injectedSkillName = '';
     let currentGroup = null;
-    let injectedPrependText = '<tabber>\n|-|GMS v223=';
+    let injectedPrependText = '<tabber>\n|-|GMS v227=';
 
     const renderSidebar = () => {
         const sidebar = document.getElementById('sidebar');
